f2440801fe02ca7abd8a63000dc5d3ac413ff75a
Ja1P <joaoprim2912@gmail.com>
2025-12-24 23:32:17 -0300

    ComitZaralhada

---
+++ urubum/camera.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+
+@onready var target = null;
+
+#func _process(_delta):
+	#global_position = target.global_position
--- urubum/enemy.gd
+++ urubum/enemy.gd
@@ -1,13 +1,41 @@
 extends RigidBody2D
 
-#Aqui vai ter os baguio do inimigo
 
-#ESCREVENDO LINHA DO INIMIGO TESTADA
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var speed = 100
+var searchSide
+@onready var spriteRend = $enemySprite;
+@onready var spritePerState = [
+preload("res://Sprites/Urubao.png"), ##IDLE -> 0 
+preload("res://Sprites/Square - Black.jpg") ##BLOCO PRETO -> 1
+];
+
+var maxTimeToFlip : int = 120;
+var timeToFlip : int = maxTimeToFlip;
+
+enum STATES { IDLE, SEARCH, JUMP }
+
+var state: STATES = STATES.IDLE
+
+func _physics_process(_delta):
+	match state:
+		STATES.IDLE:
+			pass;
+		
+		STATES.SEARCH:
+			idle()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+
+
+func idle():
+	spriteRend.texture = spritePerState.get(1);
+	timeToFlip -= 1;
+
+
+func searching():
+	if(spriteRend.flip_h == false): searchSide = 1
+	else: searchSide = -1
+	
+	self.linear_velocity = Vector2(speed * searchSide, 0);
