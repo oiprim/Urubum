ac8798c7a359e3cfcdb4e3b646576f831b9330d5
Ja1P <joaoprim2912@gmail.com>
2025-12-24 22:34:32 -0300

    ComitTeste2

--- urubum/enemy.gd
+++ urubum/enemy.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 #Aqui vai ter os baguio do inimigo
+
+#ESCREVENDO LINHA DO INIMIGO TESTADA
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
