0613243de0684e1b078b3dd0840dd2438fab339a
Ja1P <joaoprim2912@gmail.com>
2025-12-26 21:50:01 -0300

    A

--- urubum/Scripts/camera.gd
+++ urubum/Scripts/camera.gd
@@ -1,8 +1,18 @@
 extends Camera2D
 
 
+var inicialDisplaySize;
+var actualDisplaySize;
 @onready var target = $"../Player";
 
 
+func _ready():
+	inicialDisplaySize = DisplayServer.window_get_size(0)
+	pass
+
 func _process(_delta):
 	global_position = target.getPlayerPos();
+	actualDisplaySize = DisplayServer.window_get_size(0);
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	#self.offset = Vector2(inicialDisplaySize - actualDisplaySize) / 1.5;
+	
