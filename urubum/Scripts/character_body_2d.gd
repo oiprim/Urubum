342c9a810b2fb7cc5c86a0c499fd0e62b01d498a
João Prim <143097784+oiprim@users.noreply.github.com>
2026-01-01 13:31:03 -0300

    coyote time configurado (a camera ta meio bugada)

--- urubum/Scripts/character_body_2d.gd
+++ urubum/Scripts/character_body_2d.gd
@@ -5,41 +5,46 @@ extends CharacterBody2D
 @export var maxJumpHeigh : int = -1000
 @export var gravityForce : int = 2500
 
-@onready var spriteRend = $playerSprites
-const somzin = "res://hit.mp3"
+@onready var sprite_rend = $playerSprites
+@onready var coyote_timer = $CoyoteTimer
 
 func _physics_process(delta: float) -> void:
 	if velocity.x != 0:
-		spriteRend.play("run");
+		sprite_rend.play("run");
 		if velocity.x >= 0:
-			spriteRend.scale.x = 4
-		else: spriteRend.scale.x = -4
+			sprite_rend.scale.x = 4
+		else: sprite_rend.scale.x = -4
 	else:
-		spriteRend.play("idle");
+		sprite_rend.play("idle");
 
 	# Add the gravity.
 	if not is_on_floor():
 		if(velocity.y <= 0 ):
-			spriteRend.play("jump");
+			sprite_rend.play("jump");
 		elif(velocity.y >= 0):
-			spriteRend.play("fall");
+			sprite_rend.play("fall");
 		velocity += Vector2(velocity.x, gravityForce) * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Pulo
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or !coyote_timer.is_stopped()):
 		velocity.y = maxJumpHeigh
 	if Input.is_action_just_released("ui_accept") and velocity.y <0:
 		velocity.y = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Pega input e movimenta
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
+	#Coyote Time && Move and Slide
+	var was_on_floor = is_on_floor()
+	
 	move_and_slide()
+	
+	if was_on_floor &&  !is_on_floor():
+		coyote_timer.start()
 	
 func getPlayerPos():
 	return global_position;
